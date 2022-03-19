c136d7dca7eb2ff41baa631ac3efeffa3a196919
Alberto Barbosa 021 <85910024+Alberto21-boop@users.noreply.github.com>
2022-03-19 19:09:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Soma21
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 40;
+            int b = 40;
+
+            int resultado = a + b;
+
+            Console.WriteLine("O resultado da soma é " + resultado);
+        }
+    }
+}
